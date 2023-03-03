8c1549ee332efe72467fe73c05ce8c0b9816fd56
Aminur Islam Arnob <eng.aminurislam@gmail.com>
2023-03-03 21:46:22 +0600

    design done and moved from another git repo

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+(function ($) {
+    "use strict";
+    
+  //main slider
+  if ($('.slider-area').length > 0) {
+    $('.slider-area').owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 1,
+      dots: true,
+      autoplay: true,
+    });
+  }   
+    
+  
+  if ($('.new-model-slider').length > 0) {
+      $('.new-model-slider').owlCarousel({
+        loop: true,
+        margin: 55,
+        items: 2,
+        dots: true,
+        autoplay: false,
+        nav: true,
+        navText: ['＜', '＞'],
+        slideBy: 2,
+        responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          1350:{
+              items:2,
+              nav:true,
+              loop:false
+          }
+        }
+      });
+    }
+
+  //Post SLider
+  if ($('.article-slider').length > 0) {
+    $('.article-slider').owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 2,
+      dots: false,
+      autoplay: true,
+      nav: true,
+      navText: ["<span class='prev-article'>前の記事</span>","<span class='next-article'>次の記事</span>"],
+      responsive:{
+        0:{
+            items:1,
+        },
+        991:{
+            items:2,
+        }
+      }
+    });
+  }
+
+
+
+    //Mobile Menu
+    $('.mobile-menu-trigger').click(function(){
+        $('.main-menu-area, .mobile-menu-backdrop').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    $('.mobile-menu-backdrop').click(function(){
+        $('.main-menu-area, .mobile-menu-trigger').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+    
+
+    if ($(window).width() < 991) {
+      $('.main-menu li.has-submenu').append('<span class="sub-menu-trigger"><i class="fas fa-chevron-right"></i></span>');
+    }
+    $('.sub-menu-trigger').click(function () {
+      $(this).parent().find('.sub-menu').toggle();
+      $(this).toggleClass('active');
+    });
+  
+  
+}(jQuery));	
